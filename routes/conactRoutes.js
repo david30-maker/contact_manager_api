329b976c1472cd81b2da4e1395adaa96958022b7
david30-maker <davidokpah396@gmail.com>
2024-08-19 17:59:28 +0100

    create routes

---
+++ routes/conactRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const contactController = require('../controllers/contactController'); 
+
+router.get('/', contactController.getContacts);
+
+router.post('/', contactController.createContact);
+
+router.put('/:id', contactController.updateContact);
+
+router.delete('/:id', contactController.deleteContact);
+
+module.exports = router;
