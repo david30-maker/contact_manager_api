dd6d0218b76b3fe51cf85f3349b0858ca737d126
david30-maker <davidokpah396@gmail.com>
2024-08-19 08:56:55 +0100

    create post function to create new concat in database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,10 +2,29 @@ const Contact = require('../models/contact');
 
 exports.getContacts =  async (req, res) => {
     try {
-        const contact = await Contact.find();
+        const contacts = await Contact.find();
+        res.json(contacts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+exports.createContact = async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type
+        });
+
+        const contact = await newContact.save();
         res.json(contact);
     } catch (error) {
         console.error(error.message);
-        res.status(500).send('Server Error');
+        res.status(500).json({ message: 'Server Error'});
     }
 };
