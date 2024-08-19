c826097a9f034ad7b97e2b16b95a7884f8bae2aa
david30-maker <davidokpah396@gmail.com>
2024-08-19 09:06:56 +0100

    create update and delete function to create new concat in database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -28,3 +28,41 @@ exports.createContact = async (req, res) => {
         res.status(500).json({ message: 'Server Error'});
     }
 };
+
+exports.updateContact = async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ message: 'Contact not found' });
+
+        contact = await Contact.findByIdAndUpdate(req.params.id, { $set: contactFields }, { new: true });
+
+        res.json(contact);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Server Error'});
+    }
+};
+
+exports.deleteContact = async (req, res) => {
+    try {
+        const contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ message: 'Contact not found' });
+
+        await Contact.findByIdAndRemove(req.params.id);
+
+        res.json({ message: 'Contact removed' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Server Error'});
+    }
+}
+
