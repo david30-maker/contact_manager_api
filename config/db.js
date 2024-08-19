138de5653158353ed8aec4960f856d5a0a0dfa1b
david30-maker <davidokpah396@gmail.com>
2024-08-19 08:43:26 +0100

    update db.js file

--- config/db.js
+++ config/db.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      await mongoose.connect('mongodb://localhost/contact-manager-mvc', {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
+      useUnifiedTopology: true
     });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB Connected: ...');
   } catch (err) {
-    console.log(err);
+    console.error(err.message);
     process.exit(1);
   }
 };
